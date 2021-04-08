14fde14f996d209bee2bfb6b97997430a0808c03
unknown <afifmakarim88@gmail.com>
2021-04-08 14:40:50 +0700

    add regenerator runtime

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/searchBar"
+import 'regenerator-runtime'
+
 import main from "./scripts/main";
 
 main();
